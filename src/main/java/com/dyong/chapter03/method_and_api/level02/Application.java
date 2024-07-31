cd0fd8f2bf88c91a666438bf15fa6a971f5ebb7d
JIYOUNG-22 <dyong0922@naver.com>
2024-07-31 22:24:15 +0900

    chapter03.method_and_api.level02.Application

---
+++ src/main/java/com/dyong/chapter03/method_and_api/level02/Application.java
@@ -0,0 +1,13 @@
+package com.dyong.chapter03.method_and_api.level02;
+import java.util.*;
+
+public class Application {
+    public static void main(String[] args) {
+
+        System.out.println(RandomMaker.randomNumber(-300, 299));
+        System.out.println(RandomMaker.randomUpperAlpabet(9));
+        System.out.println(RandomMaker.rockPaperScissors());
+        System.out.println(RandomMaker.tossCoin());
+
+    }
+}
---
+++ src/main/java/com/dyong/chapter03/method_and_api/level02/RandomMaker.java
@@ -0,0 +1,46 @@
+package com.dyong.chapter03.method_and_api.level02;
+
+public class RandomMaker {
+
+    public static int randomNumber(int min, int max){
+        return (int) (Math.random() * (max - min) + min);
+    }
+
+    public static String randomUpperAlpabet(int length){
+        String word = "";
+        for(int i = 0; i < length; i++){
+            char alphabet = (char) (int) (Math.random() * (90-65) + 65);
+            word += alphabet;
+        }
+        return word;
+    }
+
+    public static String rockPaperScissors(){
+        int random1 = (int) (Math.random() * 3 + 1);
+        String hand = "";
+
+        if (random1 == 1){
+            hand = "가위";
+        } else if(random1 == 2){
+            hand = "바위";
+        } else {
+            hand = "보";
+        }
+
+        return hand;
+    }
+
+    public static String tossCoin(){
+        int random1 = (int) (Math.random() * 2 + 1);
+        String coin = "";
+
+        if (random1 == 1){
+            coin = "앞면";
+        } else {
+            coin = "뒷면";
+        }
+
+        return coin;
+    }
+
+}
