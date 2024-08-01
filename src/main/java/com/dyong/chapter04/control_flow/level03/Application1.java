668635143c3b6125abc158750ee44664bd78de4a
JIYOUNG-22 <dyong0922@naver.com>
2024-08-01 15:46:54 +0900

    chapter04.control_flow.level03.Application1

---
+++ src/main/java/com/dyong/chapter04/control_flow/level03/Application1.java
@@ -0,0 +1,41 @@
+package com.dyong.chapter04.control_flow.level03;
+
+import java.util.Scanner;
+
+public class Application1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("첫 번째 정수 : ");
+        int first = scanner.nextInt();
+        System.out.print("두 번째 정수 : ");
+        int second = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("연산 기호를 입력하세요 : ");
+        char ch = scanner.nextLine().charAt(0);
+
+        double result = switch (ch) {
+            case '+' -> first + second;
+            case '-' -> first - second;
+            case '*', 'x' -> first * second;
+            case '/' -> (double) first / (double) second;
+            case '%' -> first % second;
+            default -> 0;
+        };
+
+        System.out.println((result != 0) ? (first + " " + ch + " " + second + " = " + result): "입력하신 연산은 없습니다. 프로그램을 종료합니다.");
+    }
+}
+
+/* 두 개의 정수를 입력 받아 변수에 저장하고,
+ * 연산기호(+, -, *, /, %)를 입력 받아 해당 연산의 수행 결과를 출력하세요.
+ * 단, 준비된 연산기호 외의 문자를 입력하는 경우 "입력하신 연산은 없습니다. 프로그램을 종료합니다." 출력 후 프로그램 종료
+ *
+ * -- 입력 예시 --
+ * 첫 번째 정수 : 4
+ * 두 번째 정수 : 3
+ * 연산 기호를 입력하세요 : +
+ *
+ * -- 출력 예시 --
+ * 4 + 3 = 7
+ */
